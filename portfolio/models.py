454121ab116b437aaefe34605a008a20ef38843e
mohammadkanon <gentlekanon@gmail.com>
2019-07-04 20:04:47 +0600

    portolio done

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+	blog_img = models.ImageField(upload_to='blog', null=True)
+	blog_title = models.CharField(max_length=50, null=True)
+	blog_details = models.TextField(max_length=300, null=True)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from portfolio.models import Blog
 
 # Create your views here.
 def portfolio(request):
-	return render(request, 'portfolio/portfolio.html')
+
+	blog = Blog.objects.all()
+
+	context = {
+		'blog': blog
+	}
+
+	return render(request, 'portfolio/portfolio.html', context)
